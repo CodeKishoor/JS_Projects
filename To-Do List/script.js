2251fd54ce94c1a5a71e18a0dd7a088cc55abad1
KISHOORNM <kishoornmsports@gmail.com>
2025-02-12 17:24:01 +0530

    Updated

---
+++ To-Do List/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded",()=>{
+
+    const add = document.querySelector(".add");
+    const input = document.getElementById("input");
+    console.log(add,input);
+
+    add.addEventListener("click",addList);
+
+    function addList(event){
+        
+    }
+
+});
